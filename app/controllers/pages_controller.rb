18b8bce3ab6e53f937dc7919a6c871915fd4587b
oxygen545 <damon@hopkins-family.org>
2018-10-23 22:24:02 -0500

    rans scaffold on models then removed the users

--- app/controllers/pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class PagesController < InheritedResources::Base
-
-  private
-
-    def page_params
-      params.require(:page).permit(:author_id, :published_at, :page_title, :heading, :subheading, :contents, :images_id, :parents_id, :children_id)
-    end
-end
-
--- app/models/page.rb
+++
@@ -1,6 +0,0 @@
-class Page < ApplicationRecord
-  belongs_to :author
-  belongs_to :images
-  belongs_to :parents
-  belongs_to :children
-end
